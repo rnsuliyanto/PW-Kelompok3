536444bc9ee2d67fce77f049c2a9030ae82df27b
Rian Suliyanto <33859043+rnsuliyanto@users.noreply.github.com>
2024-07-24 12:05:52 +0700

    perubahan dasar awal

--- js/app.js
+++ js/app.js
@@ -10,10 +10,12 @@ $(document).ready(function () {
         $('#content').load('pages/homePage.html');
     }else if(menu == "member" ) {
         $('#content').load('pages/member.html');
-    }else if (menu == "RJS") {
-        $('#content').load('pages/RJS.html');
-    }else if (menu == "RJS2") {
-        $('#content').load('jajal/RJS2.html');
+    }else if (menu == "reactJS") {
+        $('#content').load('pages/reactJS.html');
+    }else if (menu == "jqueryJS") {
+        $('#content').load('pages/jquery.html');
+    }else if (menu == "chartsJS") {
+        $('#content').load('pages/charts.html');
     }
     });
 });
